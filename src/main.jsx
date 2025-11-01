3ded3f93e09bf267aa6f1e6b672d9248a79ddaf2
miespacioanalitico <mi.espacio.analitico@gmail.com>
2025-10-31 18:30:20 -0600

    Add main.jsx to render the App component

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './TamalesDonaOfe.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+    * Confirma los cambios (**`Commit new file`**).
